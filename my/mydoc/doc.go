a2de78fbc6a4d6dc25e5797578c8ca06fd38a50e
Dokiy <zhangzongqi@fancydigital.com.cn>
2021-11-22 16:46:41 +0800

    add myparse arrange file

---
+++ my/mydoc/doc.go
@@ -0,0 +1,11 @@
+//  git clone git@github.com:Dokiys/go_test.git
+//	In terminal run:
+//		godoc -http=:6060
+//	and check in http://localhost:8080/pkg/go_test/mydoc/
+
+/*
+	Package mydoc shows how to use go doc
+
+	This project notes from https://blog.cyeam.com/golang/2018/09/03/godoc
+ */
+package mydoc
